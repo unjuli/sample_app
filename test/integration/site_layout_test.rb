f973ed29563712b6f600a04d60271c6cdc9c6e8e
Anjali Sharma <anjali.sharma@zoomcar.com>
2015-07-29 11:51:49 +0530

    Advanced integration Test for layout links (solves exercise 9.2)

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = users(:michael)
+  end
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -11,6 +15,14 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", contact_path
     get signup_path
     assert_select "title", full_title("Sign up")
+    
+    log_in_as(@user)
+    assert_redirected_to @user
+    get users_path
+    assert_template 'users/index'
+    assert_select "a[href=?]", login_path, count = 0
+    get edit_user_path(@user)
+    assert_template 'users/edit' 
   end
 end
 
