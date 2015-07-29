fa2615c349cfa2629a243159f8fbadfbaf0c0bcf
Anjali Sharma <anjali.sharma@zoomcar.com>
2015-07-29 17:02:17 +0530

    Add condition for show user in index if activated (solves exercise 10.2)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,14 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
+
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
 
   def show
     @user = User.find(params[:id])
+    redirect_to root_url and return unless @user.activated?
   end 
   
   def new
