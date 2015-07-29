e54b59d0ef9b02be41e1c84bbac7466fe7a63804
Anjali Sharma <anjali.sharma@zoomcar.com>
2015-07-29 11:27:43 +0530

    Test for one time friendly forwarding (solves exercise 9.1)

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -32,6 +32,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     get edit_user_path(@user)
     log_in_as(@user)
     assert_redirected_to edit_user_path(@user)
+    assert_equal session[:forwarding_url], nil
     name  = "Foo Bar"
     email = "foo@bar.com"
     patch user_path(@user), user: { name:  name,
